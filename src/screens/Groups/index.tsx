eb83b8631e088d7da0fb144efd284a66392691fc
João Moreira <joaomoreira.a.s@gmail.com>
2023-12-17 08:06:55 -0300

    feat: add group card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
+import { HighLight } from "@components/HighLight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <HighLight title="Turmas" subtitle="joque com a sua turma" />
+
+      <GroupCard title="Galera do ignite" />
     </Container>
   );
 }
