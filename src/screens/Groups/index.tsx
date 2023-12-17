0b23c0ab5476c2e085348266d2873fe840b5ecec
João Moreira <joaomoreira.a.s@gmail.com>
2023-12-17 08:14:43 -0300

    add list of groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { HighLight } from "@components/HighLight";
 import { GroupCard } from "@components/GroupCard";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera do ignite"]);
+
   return (
     <Container>
       <Header />
       <HighLight title="Turmas" subtitle="joque com a sua turma" />
-
-      <GroupCard title="Galera do ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
