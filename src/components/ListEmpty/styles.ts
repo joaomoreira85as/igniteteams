4f86a361539b1db6d01464d3cbabe30657d1e232
João Moreira <joaomoreira.a.s@gmail.com>
2023-12-17 19:11:48 -0300

    refact: use styled-components css

--- src/components/ListEmpty/styles.ts
+++ src/components/ListEmpty/styles.ts
@@ -1,6 +1,6 @@
 import { UsersThree } from "phosphor-react-native";
 import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
@@ -10,7 +10,9 @@ export const Container = styled.View`
 
 export const Message = styled.Text`
   text-align: center;
-  color: ${({ theme }) => theme.COLORS.GRAY_300};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_300};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.SM}px;
+  `}
 `;
